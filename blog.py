f8c962ff2b3bf6ff6498ff8ed30fbbcc37fd8f56
Riya Sabu <riyasabures@gmail.com>
2022-12-17 20:27:28 +0530

    added formatt diff

--- blog.py
+++ blog.py
@@ -1,17 +1,17 @@
 k = int(input())
 cat_url_set = []
 while (k != 0):
-    print("Enter the category to be added: ")
-    cat = input()
-    if cat not in cat_url_set:
-        cat_url_set.append(cat)
-        i = cat_url_set.index(cat)
-        cat_url_set.append(set())
+    print("Enter the category value to be added: ")
+    value = input()
+    cat_url_set.append(set())#travel
+    cat_url_set.append(set())#food
+    cat_url_set.append(set())#tech
+    cat_url_set.append(set())#music
     print(cat_url_set)
+    index = int(value)
     print("Enter url")
     url = input()
-    i = cat_url_set.index(cat)
-    cat_url_set[i+1].add(url)
+    cat_url_set[index].add(url)
     k = k-1 
 print(cat_url_set)
 
