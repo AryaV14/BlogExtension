612d8dadb4c2a29dad6593e20711ceaec587d501
Arya Vijayan <aryavijayanrohini@gmail.com>
2022-12-18 11:40:15 +0530

    completed

--- app.py
+++ app.py
@@ -2,37 +2,47 @@ from flask import Flask, render_template, request
 app=Flask(__name__)
 
 
-
+count_visit = []
+total = []
 cat_url_set = []
 visited=[]
+percent=[]
 for _ in range(0,4):
-    cat_url_set.append(set())
+    cat_url_set.append(list())
     visited.append(list())
+    count_visit.append(0)
+    total.append(0)
+    percent.append(0)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        
         value = request.form.get('value')
-        url = request.form.get('url')
         value=int(value)
-        print(value)
+
+        url = request.form.get('url')
+
         read = request.form.get('read')
         if value != 100:
             
             if read == 'on':
                 read=1
+                count_visit[value]= count_visit[value]+1
+                print(count_visit)
+                
             else :
                 read=0
-            print(read)
+            if url not in cat_url_set[value]:
+                cat_url_set[value].append(url)
+                total[value]=total[value]+1
+                print(total)
             
-            print(url)
-            cat_url_set[value].add(url)
-            visited[value].append(read)
+                visited[value].append(read)
+            i = cat_url_set[value].index(url)
+            visited[value][i] = read
             print(cat_url_set)
             print(visited)
-            if url in cat_url_set:
-                i = cat_url_set.index(url)
-                if read == 'on':
-                    visited[i] = 1
+          
         else:
             if read == 'on':
                 read=1
@@ -40,8 +50,8 @@ def index():
                     i = cat_url_set.index(url)
                     visited[i] = 1
 
-
-        
+        percent[value]= (count_visit[value]/total[value])*100
+        print(percent)        
     return render_template('index.html')
        
 if __name__ == "__main__":
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,6 @@
-rl_set.index(url)
-        if read == 'on':
-            visited[i] = 1
+ else:
+            if read == 'on':
+                read=1
+                if url in cat_url_set:
+                    i = cat_url_set.index(url)
+                    visited[i] = 1
