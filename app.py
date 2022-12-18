af73354256972df9afbb4068ec788e367a492adf
Arya Vijayan <aryavijayanrohini@gmail.com>
2022-12-18 10:05:19 +0530

    add html

--- app.py
+++ app.py
@@ -5,21 +5,42 @@ app=Flask(__name__)
 
 cat_url_set = []
 visited=[]
+for _ in range(0,4):
+    cat_url_set.append(set())
+    visited.append(list())
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         value = request.form.get('value')
-        value=int(value)
-        cat_url_set.append(set())
-        print(value)
-
-        visited.append(list())
-
         url = request.form.get('url')
-        print(url)
-        cat_url_set[value].add(url)
-        visited[value].append(0)
-        print(cat_url_set)
+        value=int(value)
+        print(value)
+        read = request.form.get('read')
+        if value != 100:
+            
+            if read == 'on':
+                read=1
+            else :
+                read=0
+            print(read)
+            
+            print(url)
+            cat_url_set[value].add(url)
+            visited[value].append(read)
+            print(cat_url_set)
+            print(visited)
+            if url in cat_url_set:
+                i = cat_url_set.index(url)
+                if read == 'on':
+                    visited[i] = 1
+        else:
+            if read == 'on':
+                read=1
+                if url in cat_url_set:
+                    i = cat_url_set.index(url)
+                    visited[i] = 1
+
+
         
     return render_template('index.html')
        
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-       
+rl_set.index(url)
+        if read == 'on':
+            visited[i] = 1
